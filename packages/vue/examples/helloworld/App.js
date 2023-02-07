32be23448d0fa967a386516be6ab0dfde9911f9e
you_5c24 <you_5c24@163.com>
2023-02-07 14:52:46 +0800

    feat: change console

--- packages/vue/examples/helloworld/App.js
+++ packages/vue/examples/helloworld/App.js
@@ -21,7 +21,7 @@ export const App = {
       // [h("div", {}, "hi, " + this.msg), h(Foo, { count: 1 })]
       // "hi, " + this.msg
       // "hi, mini-vue"
-      [h("p", { class: "red" }, "hi,"), h("p", { class: "blue" }, "mini-vue")]
+      [h("p", { class: "red" }, "hello,"), h("p", { class: "blue" }, "mini-vue")]
     );
   },
 
